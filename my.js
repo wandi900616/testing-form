77c43e8c7f85b15148854305db076c27d6af9c93
wandi <76605635+wandi900616@users.noreply.github.com>
2021-09-10 22:36:25 +0800

    Update my.js

--- my.js
+++ my.js
@@ -9,4 +9,24 @@ function testGS(){
     
 }
 
-document.getElementById("btn").addEventListener("click",testGS)
+function addGS(){
+
+    const url="https://script.google.com/macros/s/AKfycbxyFMN1OYrsyv8PzCpoCjIScl7-4VXM5uAxpF0kyiDMKxYqsurJ/exec"
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"john"}) // body data type must match "Content-Type" header
+      })
+    
+}
+
+document.getElementById("btn").addEventListener("click",testGS);
+document.getElementById("btn2").addEventListener("click",addGS)
